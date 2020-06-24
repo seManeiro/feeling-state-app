63b76e24920fb7c080b12f73627d0b41f5b41997
seManeiro <sebastian.maneiro@gmail.com>
2020-06-24 16:49:11 +0200

    Update config.js

--- backend/configuration/config.js
+++ backend/configuration/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   db: "mongodb://localhost:27017/feelings",
   cloudDb:
-    "mongodb+srv://semaneiro:<passord>@cluster0-fbkkb.mongodb.net/<dbname>?retryWrites=true&w=majority",
+    "mongodb+srv://semaneiro:<password>@cluster0-fbkkb.mongodb.net/<dbname>?retryWrites=true&w=majority",
   secretOrKey: "secret",
 };
